b70a9fb5fedc3ca339ae2089848afb77f607dc85
Musa Godwin <musa.godwin8112@gmail.com>
2024-11-30 14:56:05 +0100

    added helper

---
+++ helpers.py
@@ -0,0 +1,4 @@
+def missing_data(data):
+    total = data.isnull().sum().sort_values(ascending = False)
+    percent = (data.isnull().sum()/data.isnull().count()*100).sort_values(ascending = False)
+    return pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
